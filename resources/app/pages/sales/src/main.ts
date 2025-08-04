c347b3a786f9c902a9d03c66496b684c7f7a0df7
Tech UO Space <techsupport@uospace.co>
2025-08-05 03:46:02 +0700

    fix: delete unused coponent

--- resources/app/pages/sales/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/index.ts";
-
-import "./assets/css/remixicon.css";
-import "./assets/css/style.css";
-
-const app = createApp(App).use(router);
-app.mount("#app");
